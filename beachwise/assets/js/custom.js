cc753c512b34f5c7374b7f293e20bb1a7ea97854
tiagosantana <t.santana@campus.fct.unl.pt>
2015-10-23 11:16:29 +0100

    delete

--- beachwise/assets/js/custom.js
+++
@@ -1,8 +0,0 @@
-		$( '.navbar-toggle' ).click(function() {
-			if( !$('.in').length ) {
-				$('.logo-img-container').css("display", "none");
-			}
-			else if( !$('.collapse').length ){
-				$('.logo-img-container').css("display", "block");
-			}
-		});
